eeb2bb218f60278807a23b088cc3f2193103ab94
Sian-soj <siansoj2005@gmail.com>
2025-10-03 23:49:36 +0530

    learned so basic datatypes and variable assigning along with how to pront the values of the variable

--- hello.c
+++ hello.c
@@ -3,5 +3,19 @@ int main(){
     printf("hi everyone\n");
     printf("I'm Sian Soj and im just a curious guy trying to learn c programming :)\n");
     printf("Hope this is easy :)\n");
+
+    // two forward slash can give me a comment
+    /*this is a
+    multiline comment*/
+
+    int age = 20;
+    float mark = 90%;
+    char letter = 'S';
+    printf("my age is %d\n", age);
+    printf("my mark is %f\n", mark);
+    printf("my letter is %c\n", letter);
+    
+    //damn vs cod can predict your entire mindset maybe :)
+    //just use tab and it will simply just guess if thats what i meant
     return 0;
 }
