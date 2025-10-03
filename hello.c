48ed9da111dce0912bdbc18e1c2ad754609ab9a7
Sian-soj <siansoj2005@gmail.com>
2025-10-03 23:35:42 +0530

    so just kinda added my intro as multiple lines of printf

--- hello.c
+++ hello.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 int main(){
-    printf("hello world\n");
+    printf("hi everyone\n");
+    printf("I'm Sian Soj and im just a curious guy trying to learn c programming :)\n");
+    printf("Hope this is easy :)\n");
     return 0;
 }
