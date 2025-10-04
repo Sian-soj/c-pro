0835768f7ecccc3d475bb758963351a3ecf1b075
Sian-soj <siansoj2005@gmail.com>
2025-10-04 23:52:24 +0530

    Starting arrays

--- day2.c
+++ day2.c
@@ -14,4 +14,23 @@ int main() {
 
   printf("The sum of a, b, and c is %f.", sum);
   return 0;
+
+
+
+  // Array time!!
+
+  int numbers[10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  printf("%d\n", numbers[0]);
+  printf("%d\n", numbers[1]);
+  printf("%d\n", numbers[2]);
+  printf("%d\n", numbers[3]);
+  printf("%d\n", numbers[4]);
+  printf("%d\n", numbers[5]);
+  printf("%d\n", numbers[6]);
+  printf("%d\n", numbers[7]);
+  printf("%d\n", numbers[8]);
+  printf("%d\n", numbers[9]);
+  return 0;
+
+  // so the thing is arrays have indexs which range from 0 to n-1 where is the range of the array
 }
