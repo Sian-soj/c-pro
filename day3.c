61b69c1ba32201e03727ebdc4d6290fd3b5fc2ee
Sian-soj <siansoj2005@gmail.com>
2025-10-06 00:44:39 +0530

    created a new array

--- day3.c
+++ day3.c
@@ -13,5 +13,23 @@ int main(){
     printf("%s\n", name[1]);
     printf("%s\n", name[2]);
 
+    // i have no clue why i was told to use 2d char array..... lemme check
+    /*damn so basically what it is saying is that 3 represents the rows and 5 represents the columns.
+    frm what i understood here 3 represents the number of items in the array
+    and 5 represents the characters.
+    here there is a special character \0 which is a null string to let the c know that this string ended*/
+
+    // cool stuff
+
+    //type name[size1][size2]...[sizeN];
+
+    int family[2][5] = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}};
+    printf("%d\n", family[0][0]);
+    printf("%d\n", family[0][1]);
+    printf("%d\n", family[0][2]);
+
     return 0;
+
+
+    // guys just remember to alwyas put return 0 at the very last end!
 }
