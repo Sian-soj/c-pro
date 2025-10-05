58809274686e1dcdcf4cc22d3c88044cf5da1545
Sian-soj <siansoj2005@gmail.com>
2025-10-05 23:51:03 +0530

    recalled single array

--- day2.c
+++ day2.c
@@ -13,13 +13,21 @@ int main() {
   sum = a + b + c ;
 
   printf("The sum of a, b, and c is %f.", sum);
-  return 0;
-
-
 
   // Array time!!
 
-  int numbers[10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // In C, array initialization uses curly braces {}
+  int numbers[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+  // A for loop is a better way to print all elements of an array
+  printf("\n\n--- Array Elements ---\n");
+  for (int i = 0; i < 10; i++) {
+    printf("%d\n", numbers[i]);
+  }
+
+  int numbers[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+  printf("\n--- Array Elements ---\n")
   printf("%d\n", numbers[0]);
   printf("%d\n", numbers[1]);
   printf("%d\n", numbers[2]);
@@ -30,7 +38,27 @@ int main() {
   printf("%d\n", numbers[7]);
   printf("%d\n", numbers[8]);
   printf("%d\n", numbers[9]);
-  return 0;
 
   // so the thing is arrays have indexs which range from 0 to n-1 where is the range of the array
+
+  /*The code below does not compile, because the grades variable is missing.
+One of the grades is missing. Can you define it so the grade average will be 85?*/
+
+  printf("\n--- Grade Average ---\n");
+
+  /* TODO: define the grades variable here */
+  int average;
+  int grades[3];
+  grades[0] = 80;
+  grades[1] = 85; // This was 75 in your code.
+  /* TODO: define the missing grade
+     so that the average will sum to 85. */
+  // To get an average of 85, the sum must be 85*3 = 255.
+  // 80 + 85 + 90 = 255.
+  grades[2] = 90; 
+
+  average = (grades[0] + grades[1] + grades[2]) / 3;
+  printf("The average of the 3 grades is: %d", average);
+
+  return 0;
 }
---
+++ day3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    //multidimensional array!!!
+    //so beforre that lemme recall what i did yesterday
+
+    int age[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+
+    printf("%d\n", age[0]);
+}
