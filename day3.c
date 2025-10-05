a09cfe8aded3b53390067491d170c19da89bdc1d
Sian-soj <siansoj2005@gmail.com>
2025-10-06 00:07:44 +0530

    learned 2d array

--- day3.c
+++ day3.c
@@ -6,4 +6,12 @@ int main(){
     int age[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
 
     printf("%d\n", age[0]);
+
+    // To store strings, you can use a 2D char array.
+    char name[3][5] = {"suji", "mama", "fifi"};
+    printf("%s\n", name[0]);
+    printf("%s\n", name[1]);
+    printf("%s\n", name[2]);
+
+    return 0;
 }
